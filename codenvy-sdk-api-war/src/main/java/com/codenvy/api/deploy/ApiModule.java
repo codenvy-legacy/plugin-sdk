5311c5100c979d977f052121f61d763b7cd29df0
Andrey Parfonov <aparfonov@codenvy.com>
2014-09-25 10:31:08 +0300

    Move dummy TokenValidator to codenvy-infrastructure-local module in platform-api project

--- codenvy-sdk-api-war/src/main/java/com/codenvy/api/deploy/ApiModule.java
+++ codenvy-sdk-api-war/src/main/java/com/codenvy/api/deploy/ApiModule.java
@@ -15,31 +15,17 @@ import com.codenvy.api.builder.BuilderSelectionStrategy;
 import com.codenvy.api.builder.BuilderService;
 import com.codenvy.api.builder.LastInUseBuilderSelectionStrategy;
 import com.codenvy.api.builder.internal.SlaveBuilderService;
-import com.codenvy.api.core.rest.ApiExceptionMapper;
-import com.codenvy.api.project.server.ProjectService;
-import com.codenvy.api.project.server.ProjectTypeService;
 import com.codenvy.api.runner.LastInUseRunnerSelectionStrategy;
 import com.codenvy.api.runner.RunnerAdminService;
 import com.codenvy.api.runner.RunnerSelectionStrategy;
 import com.codenvy.api.runner.RunnerService;
 import com.codenvy.api.runner.internal.SlaveRunnerService;
-import com.codenvy.api.user.server.TokenValidator;
 import com.codenvy.api.user.server.UserProfileService;
 import com.codenvy.api.user.server.UserService;
-import com.codenvy.api.vfs.server.ContentStreamWriter;
-import com.codenvy.api.vfs.server.RequestValidator;
-import com.codenvy.api.vfs.server.VirtualFileSystemFactory;
-import com.codenvy.api.vfs.server.search.SearcherProvider;
-import com.codenvy.ide.env.TokenValidatorImpl;
+import com.codenvy.api.workspace.server.WorkspaceService;
 import com.codenvy.ide.everrest.CodenvyAsynchronousJobPool;
 import com.codenvy.inject.DynaModule;
-import com.codenvy.runner.webapps.DeployToApplicationServerRunner;
-import com.codenvy.vfs.impl.fs.CleanableSearcherProvider;
-import com.codenvy.vfs.impl.fs.LocalFSMountStrategy;
-import com.codenvy.vfs.impl.fs.LocalFileSystemRegistryPlugin;
-import com.codenvy.vfs.impl.fs.WorkspaceHashLocalFSMountStrategy;
 import com.google.inject.AbstractModule;
-import com.google.inject.util.Providers;
 
 import org.everrest.core.impl.async.AsynchronousJobPool;
 import org.everrest.core.impl.async.AsynchronousJobService;
@@ -50,36 +36,30 @@ import org.everrest.guice.PathKey;
 public class ApiModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(ProjectService.class);
-        bind(ProjectTypeService.class);
-        bind(com.codenvy.api.project.server.ProjectImportersService.class);
-        bind(LocalFileSystemRegistryPlugin.class);
-        bind(com.codenvy.api.workspace.server.WorkspaceService.class);
-        bind(LocalFSMountStrategy.class).to(WorkspaceHashLocalFSMountStrategy.class);
-        bind(SearcherProvider.class).to(CleanableSearcherProvider.class);
-        bind(RequestValidator.class).toProvider(Providers.<RequestValidator>of(null));
-        bind(ContentStreamWriter.class).toInstance(new ContentStreamWriter());
-        bind(VirtualFileSystemFactory.class);
-        bind(ApiExceptionMapper.class).toInstance(new ApiExceptionMapper());
+        bind(WorkspaceService.class);
+
         bind(BuilderSelectionStrategy.class).toInstance(new LastInUseBuilderSelectionStrategy());
         bind(BuilderService.class);
         bind(BuilderAdminService.class);
         bind(SlaveBuilderService.class);
+
         bind(RunnerSelectionStrategy.class).toInstance(new LastInUseRunnerSelectionStrategy());
         bind(RunnerService.class);
         bind(RunnerAdminService.class);
         bind(SlaveRunnerService.class);
-        bind(DeployToApplicationServerRunner.class);
+
         bind(UserService.class);
         bind(UserProfileService.class);
+
         bind(AsynchronousJobPool.class).to(CodenvyAsynchronousJobPool.class);
         bind(new PathKey<>(AsynchronousJobService.class, "/async/{ws-id}")).to(AsynchronousJobService.class);
-        bind(TokenValidator.class).to(TokenValidatorImpl.class);
 
+        install(new com.codenvy.api.core.rest.CoreRestModule());
         install(new com.codenvy.api.analytics.AnalyticsModule());
         install(new com.codenvy.api.project.server.BaseProjectModule());
         install(new com.codenvy.api.builder.internal.BuilderModule());
         install(new com.codenvy.api.runner.internal.RunnerModule());
         install(new com.codenvy.api.vfs.server.VirtualFileSystemModule());
+        install(new com.codenvy.vfs.impl.fs.VirtualFileSystemFSModule());
     }
 }
